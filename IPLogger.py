3b277d8395d83bc4f4ab9c5b2b983f79c9494dc1
chainski <96607632+chainski@users.noreply.github.com>
2022-07-26 15:57:11 -0500

    Update IPLogger.py

--- IPLogger.py
+++ IPLogger.py
@@ -31,11 +31,11 @@ print(Colorate.Horizontal(Colors.blue_to_purple, Center.XCenter(banner)))
 Write.Print("[+] Starting IP Logger", Colors.blue_to_purple, interval=0.04)
 print()
 Write.Print("[+] Fetching IP Information . . . ", Colors.red_to_yellow, interval=0.07)
-url = ' ' # ADD YOUR IPLOGGER URL HERE
+url = " " # ADD YOUR IPLOGGER URL HERE
 headers={'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:101.0) Gecko/20100101 Firefox/101.0'} # User-Agent Can Be Changed To Whatever You Like
 response= requests.get(url.strip(), headers=headers, timeout=5)
 print()
-Write.Print("[+] IP logged successfully ! You may now check the results on Grabify", Colors.blue_to_purple, interval=0.04)
+Write.Print("[+] IP logged successfully ! You may now check the results on Grabify.", Colors.blue_to_purple, interval=0.04)
 print()
 Write.Print("[+] Thanks for using my tools <3\n", Colors.red_to_yellow, interval=0.01)
 Write.Print("[+] Press any key to continue . . .", Colors.blue_to_purple, interval=0.01)
